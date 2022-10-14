8637313ded051d42552f2672bbb42d15a5ed13d8
Alberto Blacutt <albertoblacutt@CodeFabrics-MacBook-Pro.local>
2022-10-14 11:39:46 -0400

    Added loading spinner

--- components/PrivatePage/index.js
+++ components/PrivatePage/index.js
@@ -1,9 +1,28 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Button from "@mui/material/Button";
+import styles from "./styles.module.css";
+import classnames from "classnames";
 
-export default function PrivatePage({ children }) {
-  const { data: session } = useSession();
+export default function PrivatePage({ children, fullHeight = true }) {
+  const { data: session, status } = useSession();
 
+  if (status === "loading") {
+    //TODO move this to loading component
+    return (
+      <div
+        className={classnames({
+          [styles.fullHeight]: Boolean(fullHeight),
+        })}
+      >
+        <div className={styles["sk-folding-cube"]}>
+          <div className={`${styles["sk-cube1"]} ${styles["sk-cube"]}`} />
+          <div className={`${styles["sk-cube2"]} ${styles["sk-cube"]}`} />
+          <div className={`${styles["sk-cube4"]} ${styles["sk-cube"]}`} />
+          <div className={`${styles["sk-cube3"]} ${styles["sk-cube"]}`} />
+        </div>
+      </div>
+    );
+  }
   if (session) {
     return children;
   } else {
